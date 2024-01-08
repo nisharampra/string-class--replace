de9e8481fca16c388b51de6198e2c2754c1b27f0
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-08 23:14:35 +0800

    Add files via upload

---
+++ string class -replace/string class -replace/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  string class -replace
+//
+//  Created by Nisha Ramprasath on 3/11/23.
+//
+
+#include <iostream>
+//#include <Cstiring>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    string str="prpgraming";
+    str.replace(3,6,"H");
+    cout<<str<<endl;
+    return 0;
+}
